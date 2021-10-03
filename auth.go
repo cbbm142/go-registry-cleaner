5d29b30411780a7a3d36761f648b1957370f47d3
John Pastore <cbbm142@github.com>
2021-10-03 13:08:34 +0000

    Added auth detection
    Allows switching between no auth, oauth and basic auth automatically.

---
+++ auth.go
@@ -0,0 +1,72 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"strings"
+)
+
+type authResponse struct {
+	realm, service, scope string
+}
+
+func authDetect(appConfig config) error {
+	resp, err := http.Get(appConfig.configMap["host"].(string))
+	errCheck(err)
+	switch resp.StatusCode {
+	case 200:
+		fmt.Println("noauth")
+		bearerToken = ""
+		basicAuth = false
+	case 401:
+		fmt.Println("auth")
+		if strings.Contains(resp.Header["Www-Authenticate"][0], "Bearer realm") {
+			var parsedResponse authResponse = parseAuth(resp.Header["Www-Authenticate"][0])
+			bearerToken = getToken(parsedResponse, appConfig.registryUser, appConfig.registryToken)
+		} else if strings.Contains(resp.Header["Www-Authenticate"][0], "Basic") {
+			bearerToken = ""
+			basicAuth = true
+		} else {
+			return fmt.Errorf("Unable to determine auth type")
+		}
+	default:
+		return fmt.Errorf("It appears the registry either doesn't exist, or doesn't support the v2 api.")
+	}
+	return nil
+}
+
+func getToken(auth authResponse, user string, token string) string {
+
+	var params = make(map[string]string)
+	params["realm"] = auth.realm
+	params["service"] = auth.service
+	params["scope"] = auth.scope
+	jsonParams, err := json.Marshal(&params)
+	errCheck(err)
+	req := apiReqData{
+		url:             auth.realm,
+		body:            string(jsonParams),
+		header:          []string{"Authentication"},
+		headerDirective: []string{fmt.Sprintf("%s, %s", user, token)},
+	}
+	resp := req.apiRequest()
+	body := decodeBody(resp)
+	return body.(map[string]interface{})["token"].(string)
+}
+
+func parseAuth(authHeader string) authResponse {
+	var authDirectives authResponse = authResponse{}
+	for _, directive := range strings.Split(authHeader, ",") {
+		fmt.Println(directive)
+		switch {
+		case strings.Contains(directive, "Bearer realm"):
+			authDirectives.realm = strings.Split(directive, "\"")[1]
+		case strings.Contains(directive, "service"):
+			authDirectives.service = strings.Split(directive, "\"")[1]
+		case strings.Contains(directive, "scope"):
+			authDirectives.scope = strings.Split(directive, "\"")[1]
+		}
+	}
+	return authDirectives
+}
