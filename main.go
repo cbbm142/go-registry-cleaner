a8a948b8af132d959cbe7504eb0c6a6025921cab
John Pastore <cbbm142@github.com>
2021-10-12 11:21:01 +0000

    Added https option to config
    Enable  or disable SSL connections to the registry

--- main.go
+++ main.go
@@ -15,6 +15,7 @@ type ignores struct {
 type config struct {
 	configMap        map[interface{}]interface{}
 	ignoreValues     ignores
+	httpsProtocol    bool
 	registryUser     string
 	registryPassword string
 	registryToken    string
@@ -32,12 +33,12 @@ func loadConfig(configFile string) config {
 		tags: appConfig.configMap["defaultTags"].([]interface{}),
 		days: appConfig.configMap["defaultDays"].(int),
 	}
+	appConfig.httpsProtocol = appConfig.configMap["https"].(bool)
 	_ = godotenv.Load()
 	appConfig.registryUser = os.Getenv("username")
 	appConfig.registryPassword = os.Getenv("password")
 	appConfig.registryToken = os.Getenv("token")
 	appConfig.dryRun = appConfig.configMap["dryRun"].(bool)
-	os.Setenv("registryUrl", buildUrl(appConfig.configMap["host"], appConfig.registryUser, appConfig.registryPassword))
 	return appConfig
 }
 
@@ -46,6 +47,7 @@ func main() {
 	var appConfig config = loadConfig(configFile)
 	err := authDetect(appConfig)
 	errCheck(err)
+	os.Setenv("registryUrl", buildUrl(appConfig.configMap["host"], appConfig.httpsProtocol, appConfig.registryUser, appConfig.registryPassword))
 	for _, repo := range appConfig.configMap["repos"].([]interface{}) {
 		var name string = repo.(map[string]interface{})["name"].(string)
 		if _, tagsExist := repo.(map[string]interface{})["tags"]; tagsExist {
--- main_test.go
+++ main_test.go
@@ -13,11 +13,10 @@ func TestLoadConfig(t *testing.T) {
 	os.Setenv("username", "testUser")
 	os.Setenv("password", "testPass")
 	mockConfig := loadConfig(configFile)
-	registryValue := os.Getenv("registryUrl")
 	assert.Assert(t, mockConfig.registryUser == "testUser")
-	assert.Assert(t, registryValue == "https://testUser:testPass@registry.example.com/v2/")
 	assert.Assert(t, mockConfig.ignoreValues.days == 30)
-	assert.Assert(t, reflect.ValueOf(mockConfig.configMap).Len() == 5)
+	assert.Assert(t, reflect.ValueOf(mockConfig.configMap).Len() == 6)
 	assert.Assert(t, reflect.ValueOf(mockConfig.ignoreValues.tags).Len() == 2)
 	assert.Assert(t, mockConfig.dryRun == true)
+	assert.Assert(t, mockConfig.httpsProtocol == true)
 }
